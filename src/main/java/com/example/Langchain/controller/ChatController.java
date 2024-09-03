f9ede377467a9a005c3a4d3bd60f186998756f91
hdank <149360367+hdank@users.noreply.github.com>
2024-09-03 20:19:32 +0700

    update controller, library
    add a Chat Controller to send api request to llama, add llama library to pom file

---
+++ src/main/java/com/example/Langchain/controller/ChatController.java
@@ -0,0 +1,41 @@
+package com.example.Langchain.controller;
+
+import jakarta.annotation.Nullable;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+//@RequestMapping("/api")
+//@CrossOrigin(origins = "http://localhost:4200/")
+public class ChatController {
+
+    private final OllamaChatModel chatModel;
+
+    @Autowired
+    public ChatController(OllamaChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        System.out.println(chatModel.call(message));
+        return Map.of("generation", chatModel.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatModel.stream(prompt);
+    }
+}
